d140e452a8505e69dd515c29b0a8b756043ec435
Konrad Piascik <kpiascik@magicleap.com>
2019-03-05 15:13:10 -0500

    Updated unit tests for full coverage of all commands

--- __tests__/build.js
+++ __tests__/build.js
@@ -14,6 +14,7 @@ const build = require("../commands/build");
 
 beforeEach(() => {
   mockedFs.existsSync = jest.fn();
+  mockedFs.mkdirSync = jest.fn();
   mockedFs.readdirSync = jest.fn();
 });
 
@@ -26,6 +27,9 @@ afterEach(() => {
 describe("Test build", () => {
   test("error npm run", () => {
     mockedFs.existsSync.mockReturnValue(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm run build");
       expect(() => callback("error")).toThrow();
@@ -35,6 +39,9 @@ describe("Test build", () => {
 
   test("error mabu", () => {
     mockedFs.existsSync.mockReturnValue(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm run build");
@@ -49,6 +56,9 @@ describe("Test build", () => {
 
   test("no error no install", () => {
     mockedFs.existsSync.mockReturnValue(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm run build");
@@ -63,6 +73,9 @@ describe("Test build", () => {
 
   test("no error mldb install", () => {
     mockedFs.existsSync.mockReturnValue(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm run build");
@@ -86,6 +99,9 @@ describe("Test build", () => {
 
   test("error mldb install", () => {
     mockedFs.existsSync.mockReturnValue(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm run build");
@@ -109,6 +125,9 @@ describe("Test build", () => {
 
   test("error npm install", () => {
     mockedFs.existsSync.mockReturnValue(false);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm install");
@@ -119,6 +138,9 @@ describe("Test build", () => {
 
   test("no error npm install", () => {
     mockedFs.existsSync.mockReturnValue(false);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
     util.createDigest = jest.fn().mockReturnValue(false);
     child_process.exec.mockImplementationOnce((command, callback) => {
       expect(command).toBe("npm install");
@@ -128,5 +150,71 @@ describe("Test build", () => {
       callback(null);
     });
     build({ "_": ["build"], "install": true });
+  });
+
+  test("readdir no package", () => {
+    mockedFs.existsSync.mockReturnValueOnce(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["test.mabu"];
+    });
+    util.createDigest = jest.fn().mockReturnValueOnce(false);
+    child_process.exec.mockImplementationOnce((command, callback) => {
+      expect(command).toBe("npm run build");
+      child_process.exec.mockImplementationOnce((command, callback) => {
+        expect(command).toBe("mabu -t device app.package");
+      });
+      callback(null);
+    });
+    build({ "_": ["build"], "install": false });
+  });
+
+  test("mkdir error EEXIST", () => {
+    mockedFs.existsSync.mockReturnValueOnce(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
+    mockedFs.mkdirSync.mockImplementationOnce(() => {
+      let err = Error("EEXIST");
+      err.code = "EEXIST";
+      throw err;
+    });
+    util.createDigest = jest.fn().mockReturnValueOnce(false);
+    child_process.exec.mockImplementationOnce((command, callback) => {
+      expect(command).toBe("npm run build");
+      child_process.exec.mockImplementationOnce((command, callback) => {
+        expect(command).toBe("mabu -t device app.package");
+      });
+      callback(null);
+    });
+    build({ "_": ["build"], "install": false });
+  });
+
+  test("mkdir error other", () => {
+    mockedFs.existsSync.mockReturnValueOnce(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      return ["app.package"];
+    });
+    mockedFs.mkdirSync.mockImplementationOnce(() => {
+      let err = Error("other");
+      err.code = "other";
+      throw err;
+    });
+    util.createDigest = jest.fn().mockReturnValueOnce(false);
+    child_process.exec.mockImplementationOnce((command, callback) => {
+      expect(command).toBe("npm run build");
+    });
+    expect(() => build({ "_": ["build"], "install": false })).toThrow();
+  });
+
+  test("readdir error", () => {
+    mockedFs.existsSync.mockReturnValueOnce(true);
+    mockedFs.readdirSync.mockImplementationOnce(() => {
+      throw new Error();
+    });
+    util.createDigest = jest.fn().mockReturnValueOnce(false);
+    child_process.exec.mockImplementationOnce((command, callback) => {
+      expect(command).toBe("npm run build");
+    });
+    expect(() => build({ "_": ["build"], "install": false })).toThrow();
   });
 });
