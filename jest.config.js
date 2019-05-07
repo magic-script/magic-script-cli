46addb323bb8cf1d137e7bd83d743d08f78a6b40
Konrad Piascik <kpiascik@magicleap.com>
2019-05-07 11:52:21 -0400

    Update to use Apache 2.0 License

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
-// Distributed under MIT License. See LICENSE file in the project root for full license information.
+// Distributed under Apache 2.0 License. See LICENSE file in the project root for full license information.
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
