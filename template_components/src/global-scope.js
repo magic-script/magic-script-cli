212e552ac22773bbebbb2bcfbee3894dcfda326f
Leonid Zolotarev <lzolotarev@magicleap.com>
2019-09-03 15:14:28 -0400

    Update "Components" template

--- template_components/src/global-scope.js
+++ template_components/src/global-scope.js
@@ -1,3 +1,3 @@
 // This is a required workaround which expects NODE_ENV
-globalThis.process = { env: { NODE_ENV:"development" } };
+globalThis.process = { env: { NODE_ENV: "development" } };
 export default process;
