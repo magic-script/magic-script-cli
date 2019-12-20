69b04962ca4efe5a070bf5038291f2b75ac16e1c
panwrona <mariusz.brona@gmail.com>
2019-12-20 12:11:04 +0100

    Fix global-scope import

--- template_multiplatform_components/lumin/src/main.js
+++ template_multiplatform_components/lumin/src/main.js
@@ -2,7 +2,7 @@
 // Simply importing this sets all these as global definitions.
 // They are declared in the .eslintrc so your editor won't complain.
 import "magic-script-polyfills";
-import process from "./global-scope.js.js";
+import process from "./global-scope.js";
 import React from "react";
 import mxs from "magic-script-components-lumin";
 
