ac7036c7f25541e1cb47b3e7d1c1b58126ad71e6
Tim Caswell <tcaswell@magicleap.com>
2020-03-31 11:36:42 -0500

    Update typescript template for 1.7.0 typings

--- template_overlay_typescript/src/app.ts
+++ template_overlay_typescript/src/app.ts
@@ -1,32 +1,37 @@
 import { LandscapeApp, ui } from 'lumin';
 const { UiText, EclipseLabelType, Alignment, HorizontalTextAlignment } = ui;
 
+function check<T>(val: T | null, message?: string): T {
+  if (val === null) throw new Error(message || "Unexpected null encountered")
+  return val;
+}
+
 export class App extends LandscapeApp {
-  onAppStart () {
+  onAppStart() {
     // Create a new prism that's half a meter cubed.
-    let prism = this.requestNewPrism([0.5, 0.5, 0.5]);
+    let prism = check(this.requestNewPrism([0.5, 0.5, 0.5]));
 
     // Create a nice text label using UIKit.
-    let text = UiText.CreateEclipseLabel(
+    let text = check(UiText.CreateEclipseLabel(
       prism,
       'Hello\nMagicScript!',
       EclipseLabelType.kT7
-    );
+    ));
     text.setAlignment(Alignment.CENTER_CENTER);
     text.setTextAlignment(HorizontalTextAlignment.kCenter);
 
     // Attach the label to the root of the prism's scene graph.
-    prism.getRootNode().addChild(text);
+    check(prism.getRootNode()).addChild(text);
   }
 
   // Known Issue
   // Web Inspector does not work unless the updateLoop function is present in source.
   // It can be removed for production code.
-  updateLoop (delta: number) {
+  updateLoop(delta: number) {
     return true;
   }
 
-  init () {
+  init() {
     return 0;
   }
 }
