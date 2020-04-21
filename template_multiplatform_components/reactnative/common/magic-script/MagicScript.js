1f649368c67a26235847f0da0c6a2908262fab48
panwrona <mariusz.brona@gmail.com>
2020-04-21 13:02:43 +0200

    Add file system access for magic-script-components-react-native and enable plane detection out of a box

--- template_multiplatform_components/reactnative/common/magic-script/MagicScript.js
+++ template_multiplatform_components/reactnative/common/magic-script/MagicScript.js
@@ -1,9 +1,12 @@
 import { AppRegistry } from 'react-native';
-import { ReactNativeMagicScript } from 'magic-script-components-react-native';
+import { ReactNativeMagicScript, NativeFileSystem, NativePlaneDetector } from 'magic-script-components-react-native';
 import ReactNativeApp from '../react-native/ReactNativeApp';
+import { FileSystem, PlaneDetector } from 'magic-script-components';
 
 const MagicScript = {
     registerApp: (name, appComponent, debug = false) => {
+        PlaneDetector.setNativePlaneDetector(new NativePlaneDetector());
+        FileSystem.setNativeFileSystem(new NativeFileSystem());
         AppRegistry.registerComponent(name, () => ReactNativeApp);
         ReactNativeMagicScript.render(appComponent, { name: 'root' }, null, debug);
     }
--- template_multiplatform_components/src/app.js
+++ template_multiplatform_components/src/app.js
@@ -1,4 +1,3 @@
-//
 import React from 'react';
 import { View, Text } from 'magic-script-components';
 
